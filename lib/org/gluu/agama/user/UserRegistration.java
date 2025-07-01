7eb423f02a6f0fdc989d7501ddf4144a1f113e6e
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-07-01 23:16:13 +0530

    refactor: update UserRegistration.java

--- lib/org/gluu/agama/user/UserRegistration.java
+++ lib/org/gluu/agama/user/UserRegistration.java
@@ -2,18 +2,23 @@ package org.gluu.agama.user;
 
 import java.util.Map;
 
-import org.gluu.agama.registration.JansUserRegistration;
-
-
-
 public abstract class UserRegistration {
+
     public abstract String addNewUser(Map<String, String> profile) throws Exception;
 
     public abstract boolean usernamePolicyMatch(String userName);
 
     public abstract boolean passwordPolicyMatch(String userPassword);
-    
+
+    public abstract boolean sendSmsOtp(String phoneNumber);
+
+    public abstract boolean validateSmsOtp(String phoneNumber, String otp);
+
+    public abstract boolean sendEmailOtp(String email);
+
+    public abstract boolean validateEmailOtp(String email, String otp);
+
     public static UserRegistration getInstance(){
-        return  JansUserRegistration.getInstance();
-    }    
+        return JansUserRegistration.getInstance();
+    }
 }
